89c3ce3135fd33777b6aaa33e4ff779ab74c00fa
AsgerJon <asgerjon2@gmail.com>
2023-05-12 22:55:23 +0200

    Added setup and changes

---
+++ src/setup.py
@@ -0,0 +1,16 @@
+#  MIT License
+#  Copyright (c) 2023 Asger Jon Vistisen
+
+from setuptools import setup
+
+setup(
+  name='WorkToy',
+  version='0.1',
+  packages=['worktoy', 'worktoy.mockdata'],
+  package_dir={'': 'src'},
+  url='https://github.com/AsgerJon/WorkToy',
+  license='MIT',
+  author='Asger Jon Vistisen',
+  author_email='asgerjon2@gmail.com',
+  description='Collection of General Utilities  '
+)
