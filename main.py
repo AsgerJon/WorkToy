0142822e0da851f85154d9abea10c0dafe981826
AsgerJon <asgerjon2@gmail.com>
2024-07-09 23:24:05 +0200

    Consolidation of packages

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from __future__ import annotations
 import os
 import sys
 import time
-import unittest
-from typing import Never, Any, Callable
+from typing import Never, Any
 
 from icecream import ic
+
+from worktoy.yolo import yolo, runTests
 
 ic.configureOutput(includeContext=True)
 
@@ -34,54 +35,5 @@ def tester01() -> int:
   return 0
 
 
-def runTests() -> int:
-  """Runs the tests"""
-  results = []
-  loader = unittest.TestLoader()
-  res = None
-  for item in os.listdir('tests'):
-    if not item.startswith('test'):
-      continue
-    testPath = os.path.join('tests', item)
-    suite = loader.discover(start_dir=testPath, pattern='test*.py')
-    runner = unittest.TextTestRunner(verbosity=0)
-    res = runner.run(suite)
-    if res.wasSuccessful():
-      results.append('Tests passed in: %s' % testPath)
-    else:
-      results.append('Tests failed in: %s' % testPath)
-  for result in results:
-    print(result)
-  if res is None:
-    return -1
-  return 0
-
-
-def main(*args: Callable) -> None:
-  """Main Tester Script"""
-  tic = time.perf_counter_ns()
-  print('Running python script located at: \n%s' % sys.argv[0])
-  print('Started at: %s' % time.ctime())
-  print(77 * '-')
-  retCode = 0
-  for callMeMaybe in args:
-    print('\nRunning: %s\n' % callMeMaybe.__name__)
-    try:
-      retCode = callMeMaybe()
-    except BaseException as exception:
-      print('Exception: %s' % exception)
-      retCode = -1
-  retCode = 0 if retCode is None else retCode
-  print(77 * '-')
-  print('Return Code: %s' % retCode)
-  toc = time.perf_counter_ns() - tic
-  if toc < 1000:
-    print('Runtime: %d nanoseconds' % (int(toc),))
-  elif toc < 1000000:
-    print('Runtime: %d microseconds' % (int(toc * 1e-03),))
-  elif toc < 1000000000:
-    print('Runtime: %d milliseconds' % (int(toc * 1e-06),))
-
-
 if __name__ == '__main__':
-  main(runTests, )
+  yolo(runTests, tester00)
---
+++ src/worktoy/yolo/_yolo.py
@@ -0,0 +1,34 @@
+"""The 'yolo' function receives any number of callables and runs them."""
+#  AGPL-3.0 license
+#  Copyright (c) 2024 Asger Jon Vistisen
+from __future__ import annotations
+
+import sys
+import time
+from typing import Callable
+
+
+def yolo(*args: Callable) -> None:
+  """The 'yolo' function receives any number of callables and runs them."""
+  tic = time.perf_counter_ns()
+  print('Running python script located at: \n%s' % sys.argv[0])
+  print('Started at: %s' % time.ctime())
+  print(77 * '-')
+  retCode = 0
+  for callMeMaybe in args:
+    print('\nRunning: %s\n' % callMeMaybe.__name__)
+    try:
+      retCode = callMeMaybe()
+    except BaseException as exception:
+      print('Exception: %s' % exception)
+      retCode = -1
+  retCode = 0 if retCode is None else retCode
+  print(77 * '-')
+  print('Return Code: %s' % retCode)
+  toc = time.perf_counter_ns() - tic
+  if toc < 1000:
+    print('Runtime: %d nanoseconds' % (int(toc),))
+  elif toc < 1000000:
+    print('Runtime: %d microseconds' % (int(toc * 1e-03),))
+  elif toc < 1000000000:
+    print('Runtime: %d milliseconds' % (int(toc * 1e-06),))
