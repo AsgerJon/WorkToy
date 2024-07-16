6ce355022676167c518530e7348040880c546056
AsgerJon <asgerjon2@gmail.com>
2024-07-16 17:19:20 +0200

    Moved statistics and threading to dedicated branches. The main branch should contain only release ready modules.

--- src/worktoy/threading/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Threading implements support for multi-threading."""
-#  AGPL-3.0 license
-#  Copyright (c) 2024 Asger Jon Vistisen
-from __future__ import annotations
-
-from ._abstract_line import AbstractLine
