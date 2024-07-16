847ba2cfb10b4b2ca2c5a61e414c7bfcd08db473
AsgerJon <asgerjon2@gmail.com>
2024-07-16 17:12:06 +0200

    Moved work on statistics and math to separate thread.

--- src/worktoy/stream/__init__.py
+++
@@ -1,7 +0,0 @@
-"""The 'worktoy.stream' package provides classes and functions for working
-with streams of data. """
-#  AGPL-3.0 license
-#  Copyright (c) 2024 Asger Jon Vistisen
-from __future__ import annotations
-
-from ._listener import Listener
