7e261b302ffe9fd0c85bfa70028a0de1b3ce2347
AsgerJon <asgerjon2@gmail.com>
2024-07-18 05:01:51 +0200

    Removed WIP features from main branch

--- src/worktoy/loremify/__init__.py
+++
@@ -1,7 +0,0 @@
-"""The 'worktoy.loremify' package provides a Loremify class for generating
-lorem ipsum text!"""
-#  AGPL-3.0 license
-#  Copyright (c) 2024 Asger Jon Vistisen
-from __future__ import annotations
-
-from ._vocabulary import Vocabulary
--- src/worktoy/math/_base_types.py
+++
@@ -1,6 +0,0 @@
-"""This file provides type-names."""
-#  AGPL-3.0 license
-#  Copyright (c) 2024 Asger Jon Vistisen
-from __future__ import annotations
-
-Number = float | complex
