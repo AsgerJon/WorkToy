c6747642f66210158ed54c90614b86d3e29f31b4
AsgerJon <asgerjon2@gmail.com>
2024-09-09 23:57:49 +0200

    Implementation of global __init__

--- src/worktoy/__init__.py
+++ src/worktoy/__init__.py
@@ -1,7 +1,8 @@
 """The 'worktoy' package provides a collection of utilities leveraging
 advanced python features including custom metaclasses and the descriptor
 protocol. The readme file included provides detailed documentation on the
-included features. """
+included features. The modules provided depend on each other in
+implementation, but can be used independently. """
 #  AGPL-3.0 license
 #  Copyright (c) 2024 Asger Jon Vistisen
 from __future__ import annotations
