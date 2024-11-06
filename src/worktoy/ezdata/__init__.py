b1e60c0fc0363b7dc2e7f5fd684724b8d33a6738
AsgerJon <asgerjon2@gmail.com>
2024-11-06 11:47:06 +0100

    EZData being developed

--- src/worktoy/ezdata/__init__.py
+++ src/worktoy/ezdata/__init__.py
@@ -4,7 +4,3 @@ inline. """
 #  AGPL-3.0 license
 #  Copyright (c) 2024 Asger Jon Vistisen
 from __future__ import annotations
-
-from ._ez_space import EZSpace
-from ._ez_meta import EZMeta
-from ._ez_data import EZData
--- tests/test_ezdata/test_ezdata.py
+++ tests/test_ezdata/test_ezdata.py
@@ -2,24 +2,3 @@
 #  AGPL-3.0 license
 #  Copyright (c) 2024 Asger Jon Vistisen
 from __future__ import annotations
-
-from unittest import TestCase
-
-from worktoy.ezdata import EZData
-
-
-class Complex(EZData):
-  """Complex encapsulates complex numbers in a dataclass"""
-
-
-class TestEZData(TestCase):
-  """TestEZData tests the functionality of EZData"""
-
-  def setUp(self) -> None:
-    """Sets up each method"""
-    self.ezData = Complex()
-    self.inlineClass = EZData()
-
-  def testInit(self, ) -> None:
-    """Tests the initialization of the EZData class"""
-    self.assertEqual(self.ezData.IM, 0.0)
