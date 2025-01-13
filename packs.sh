3cecb760fbf884c0ac851a6b40489bc1073ada64
Asger Jon Vistisen <asgerjon2@gmail.com>
2025-01-13 18:56:49 +0100

    Update

---
+++ packs.sh
@@ -0,0 +1,3 @@
+pacstrap /mnt base linux linux-firmware base-devel vim networkmanager \
+netctl dhcpcd grub os-prober efibootmgr sudo openssh git dialog \
+man-db man-pages terminus-font zsh noto-fonts
