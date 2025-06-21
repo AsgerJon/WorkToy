4ddb76eb1922bcc9879b9fdadd8411ccfe475f14
AsgerJon <asgerjon2@gmail.com>
2025-06-21 11:42:30 +0200

    Removing deprecated code and expanding documentation

--- src/deprecated/quick/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Complex numbers example."""
-#  AGPL-3.0 license
-#  Copyright (c) 2025 Asger Jon Vistisen
-from __future__ import annotations
-
-from ._complex_numbers import ComplexNumber, main
