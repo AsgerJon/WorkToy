1f53d5b211be7c05e8f066193dc8bf4b66634a94
AsgerJon <asgerjon2@gmail.com>
2025-07-06 08:28:38 +0200

    100 % coverage achieved, removed icecream and unused imports

--- packs.sh
+++
@@ -1,3 +0,0 @@
-pacstrap /mnt base linux linux-firmware base-devel vim networkmanager \
-netctl dhcpcd grub os-prober efibootmgr sudo openssh git dialog \
-man-db man-pages terminus-font zsh noto-fonts
