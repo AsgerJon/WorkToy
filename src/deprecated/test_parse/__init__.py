b1b6b36d613114fcad24a8d7d8b7c3ac0b65dae5
AsgerJon <asgerjon2@gmail.com>
2025-07-11 07:26:54 +0200

    Remove support for type hinting slots in EZData classes

--- src/deprecated/test_parse/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-The 'test_parse' module provides unit tests for the 'worktoy.parse' module.
-"""
-#  AGPL-3.0 license
-#  Copyright (c) 2025 Asger Jon Vistisen
-from __future__ import annotations
