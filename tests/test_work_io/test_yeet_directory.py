b073fef36d4fa2babb44e9ef01a68c8b59b20771
AsgerJon <asgerjon2@gmail.com>
2026-01-28 11:27:03 +0100

    Renamed write-once descriptor 'LabelBox' to 'FixBox'

--- tests/test_work_io/test_yeet_directory.py
+++ tests/test_work_io/test_yeet_directory.py
@@ -3,7 +3,7 @@ TestYeetDirectory tests the yeetDirectory function from the
 'worktoy.work_io' module.
 """
 #  AGPL-3.0 license
-#  Copyright (c) 2025 Asger Jon Vistisen
+#  Copyright (c) 2025-2026 Asger Jon Vistisen
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
