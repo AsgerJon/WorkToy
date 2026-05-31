228153633151c67b4f95a6fb6f4f5e239bbe840b
Asger Jon Vistisen <asgerjon2@gmail.com>
2026-05-31 13:05:38 +0000

    rc 1.0.0-rc8

--- src/worktoy/__init__.py
+++ src/worktoy/__init__.py
@@ -38,7 +38,7 @@ chain.
 #  Copyright (c) 2025-2026 Asger Jon Vistisen
 from __future__ import annotations
 
-__version__ = '1.0.0-rc7'
+__version__ = '1.0.0-rc8'
 
 from . import utilities
 from . import waitaminute
